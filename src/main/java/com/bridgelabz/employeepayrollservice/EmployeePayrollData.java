662dc6e30ccc2d7de2f7284b470ff726e85f1443
Kauk777 <curokky777@gmail.com>
2020-10-14 20:10:10 +0530

    [KAUSHAL]Added: UC1 reading and writing employee data in console

---
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollData.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.employeepayrollservice;
+
+public class EmployeePayrollData {
+	private int employeeId;
+	private String employeeName;
+	private double employeeSalary;
+
+	public EmployeePayrollData(int employeeId, String employeeName, double employeeSalary) {
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeSalary = employeeSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeePayrollData [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeSalary="
+				+ employeeSalary + "]";
+	}
+
+}
