58ce7ccd76948f336930399708809a220547e512
Kauk777 <curokky777@gmail.com>
2020-10-15 15:40:51 +0530

    [KAUSHAL]Added: UC5 printing and counting data emtries

--- src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
@@ -17,8 +17,8 @@ public class EmployeePayrollServiceTest {
 		employeePayrollService=new EmployeePayrollService(Arrays.asList(arrayOfEmps));
 		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
 		employeePayrollService.printData(IOService.FILE_IO);
-		/*long entries=employeePayrollService.countEntries(IOService.FILE_IO);
-		Assert.assertEquals(3, entries);*/
+		long entries=employeePayrollService.countEntries(IOService.FILE_IO);
+		Assert.assertEquals(3, entries);
 	}
 
 }
